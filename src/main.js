184cb4664495c1f0e4d6b839f10f1c07b3d56360
wuqian <1307144731@qq.com>
2019-01-25 18:32:40 +0800

    vue3.0移动端基本

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue';
+import FastClick from 'fastclick';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import './vux-ui';
 
 Vue.config.productionTip = false;
+FastClick.attach(document.body);
 
 new Vue({
   router,
---
+++ vue.config.js
@@ -0,0 +1,82 @@
+/**
+ * @param {string} baseUrl 公共路径
+ * @param {string} outputDir 输出目录
+ * @param {string} assetsDir 用于嵌套生成静态资产的目录
+ * @param {string} productionSourceMap 是否构建map文件
+ */
+const CompressionWebpackPlugin = require('compression-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const vuxLoader = require('vux-loader');
+
+const productionGzipExtensions = ['js', 'css'];
+const path = require('path');
+
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
+
+module.exports = {
+  baseUrl: '/',
+  outputDir: 'dist',
+  lintOnSave: process.env.NODE_ENV !== 'production',
+  assetsDir: 'static',
+  productionSourceMap: false,
+  devServer: {
+    host: '0.0.0.0',
+    port: 8080,
+    proxy: {
+      '/api': {
+        target: 'http://192.168.10.217:8282/',
+        ws: true,
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': '/',
+        },
+      },
+    },
+    overlay: {
+      warnings: true,
+      errors: true,
+    }, // 浏览器 overlay 同时显示警告和错误：
+  },
+  chainWebpack: (config) => {
+    config.resolve.alias.set('@', resolve('src'));
+  },
+  configureWebpack: (config) => {
+    if (process.env.NODE_ENV === 'production') {
+      config.plugins.push(
+        vuxLoader.merge(config, {
+          options: {},
+          plugins: ['vux-ui'],
+        }),
+        new UglifyJsPlugin({
+          uglifyOptions: {
+            compress: {
+              warnings: false,
+              drop_debugger: true,
+              drop_console: true,
+            },
+            output: {
+              comments: false,
+            },
+          },
+          sourceMap: false,
+          parallel: true,
+        }),
+        new CompressionWebpackPlugin({
+          filename: '[path].gz[query]',
+          algorithm: 'gzip',
+          test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'), // eslint-disable-line
+          threshold: 10240,
+          minRatio: 0.8,
+        }),
+      );
+    }
+  },
+  css: {
+    extract: true, // 是否使用css分离插件 ExtractTextPlugin
+    sourceMap: false, // 是否在构建样式地图，false将提高构建速度
+    loaderOptions: {}, // css预设器配置项
+    modules: false, // 启用 CSS modules for all css / pre-processor files.
+  },
+};
