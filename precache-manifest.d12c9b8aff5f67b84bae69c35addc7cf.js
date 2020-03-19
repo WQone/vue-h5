5f080d0a18afd097bc2303c43074c816b0858c7e
Travis CI User <travis@example.org>
2020-03-19 02:39:20 +0000

    Update tools

---
+++ precache-manifest.d12c9b8aff5f67b84bae69c35addc7cf.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ac3af9afad85f3893c6",
+    "url": "static/css/app.2e6e21dc.css"
+  },
+  {
+    "revision": "1ac3af9afad85f3893c6",
+    "url": "static/js/app.d3306cc0.js"
+  },
+  {
+    "revision": "aac55a3abd7611a97afa",
+    "url": "static/css/chunk-07b34015.cee502d0.css"
+  },
+  {
+    "revision": "aac55a3abd7611a97afa",
+    "url": "static/js/chunk-07b34015.13703bcf.js"
+  },
+  {
+    "revision": "d035b20126d0bef2fe4f",
+    "url": "static/js/chunk-2d2248c3.cef8e702.js"
+  },
+  {
+    "revision": "f391d686d7bcceaa0235",
+    "url": "static/js/chunk-2d2378e6.5594af0d.js"
+  },
+  {
+    "revision": "8b3b041ccb6fdc0678e1",
+    "url": "static/css/chunk-vendors.a8889e05.css"
+  },
+  {
+    "revision": "8b3b041ccb6fdc0678e1",
+    "url": "static/js/chunk-vendors.68dadbb0.js"
+  },
+  {
+    "revision": "7a1ee6db87af65e71ab10a796063bf52",
+    "url": "static/img/icon_loading.7a1ee6db.png"
+  },
+  {
+    "revision": "4252a96183a5fb08100f9a96e9ac6249",
+    "url": "static/img/timg.4252a961.jpg"
+  },
+  {
+    "revision": "c7cf127fe315c799fb9ba878bfb999e5",
+    "url": "index.html"
+  },
+  {
+    "revision": "4c002cb6a82140d68d124638b17891a6",
+    "url": "favicon1.ico"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+];
